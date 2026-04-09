a056ad2bbcaa77d09ca96ceedfe19c57b5172429
Gabrax <gab-oz@wp.pl>
2026-04-09 16:31:13 +0200

    raylib + jolt

--- README.md
+++ README.md
@@ -19,17 +19,11 @@
 <div align="center">
 
 <p>
-<a href="https://github.com/glfw/glfw">glfw</a> •
 <a href="https://github.com/g-truc/glm">glm</a> •
 <a href="https://github.com/libsndfile/libsndfile">libsndfile</a> •
 <a href="https://github.com/kcat/openal-soft">OpenAL-Soft</a> •
-<a href="https://github.com/nothings/stb/blob/master/stb_image.h">stb_image</a> •
-<a href="https://github.com/freetype/freetype">freetype</a> •
-<a href="https://github.com/assimp/assimp">assimp</a> •
-<a href="https://github.com/zeux/meshoptimizer">meshoptimizer</a> •
 <a href="https://github.com/nlohmann/json">nlohmann_json</a> •
-<a href="https://github.com/jrouwe/JoltPhysics">Jolt Physics</a> •
-<a href="https://github.com/bkaradzic/metal-cpp#">metal-cpp</a>
+<a href="https://github.com/jrouwe/JoltPhysics">Jolt Physics</a>
 </p>
 
 </div>
--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,13 @@ extern "C" {
 }
 #endif
 
-#include <iostream>
-#include <thread>
-
-#include "Metal.hpp"
+#include <raylib.h>
 
 #include <Jolt/Jolt.h>
 #include <Jolt/RegisterTypes.h>
-
 #include <Jolt/Core/Factory.h>
 #include <Jolt/Core/TempAllocator.h>
 #include <Jolt/Core/JobSystemThreadPool.h>
-
 #include <Jolt/Physics/PhysicsSystem.h>
 #include <Jolt/Physics/Body/BodyCreationSettings.h>
 #include <Jolt/Physics/Collision/Shape/BoxShape.h>
@@ -25,125 +20,167 @@ extern "C" {
 using namespace JPH;
 
 namespace Layers {
-	static constexpr ObjectLayer NON_MOVING = 0;
-	static constexpr ObjectLayer MOVING = 1;
-	static constexpr uint NUM_LAYERS = 2;
+    static constexpr ObjectLayer NON_MOVING = 0;
+    static constexpr ObjectLayer MOVING = 1;
+    static constexpr uint NUM_LAYERS = 2;
 }
 
 class BroadPhaseLayerInterfaceImpl final : public BroadPhaseLayerInterface {
 public:
-	BroadPhaseLayerInterfaceImpl() {
-		mObjectToBroadPhase[Layers::NON_MOVING] = BroadPhaseLayer(0);
-		mObjectToBroadPhase[Layers::MOVING] = BroadPhaseLayer(1);
-	}
+    BroadPhaseLayerInterfaceImpl() {
+        mObjectToBroadPhase[Layers::NON_MOVING] = BroadPhaseLayer(0);
+        mObjectToBroadPhase[Layers::MOVING] = BroadPhaseLayer(1);
+    }
 
-	uint GetNumBroadPhaseLayers() const override {
-		return 2;
-	}
+    [[nodiscard]] uint GetNumBroadPhaseLayers() const override {
+        return 2;
+    }
 
-	BroadPhaseLayer GetBroadPhaseLayer(ObjectLayer inLayer) const override {
-		return mObjectToBroadPhase[inLayer];
-	}
+    [[nodiscard]] BroadPhaseLayer GetBroadPhaseLayer(ObjectLayer inLayer) const override {
+        return mObjectToBroadPhase[inLayer];
+    }
 
 #if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
-	const char* GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const override {
-		switch (inLayer.GetValue()) {
-			case 0: return "NON_MOVING";
-			case 1: return "MOVING";
-			default: return "UNKNOWN";
-		}
-	}
+    const char* GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const override {
+        switch (inLayer.GetValue()) {
+            case 0: return "NON_MOVING";
+            case 1: return "MOVING";
+            default: return "UNKNOWN";
+        }
+    }
 #endif
 
 private:
-	BroadPhaseLayer mObjectToBroadPhase[Layers::NUM_LAYERS];
+    BroadPhaseLayer mObjectToBroadPhase[Layers::NUM_LAYERS]{};
 };
 
 class ObjectLayerPairFilterImpl : public ObjectLayerPairFilter {
 public:
-	bool ShouldCollide(ObjectLayer inLayer1, ObjectLayer inLayer2) const override {
-		return true;
-	}
+    [[nodiscard]] bool ShouldCollide(ObjectLayer inLayer1, ObjectLayer inLayer2) const override {
+        return true;
+    }
 };
 
 class ObjectVsBroadPhaseLayerFilterImpl : public ObjectVsBroadPhaseLayerFilter {
 public:
-	bool ShouldCollide(ObjectLayer, BroadPhaseLayer) const override {
-		return true;
-	}
+    [[nodiscard]] bool ShouldCollide(ObjectLayer, BroadPhaseLayer) const override {
+        return true;
+    }
 };
 
 int main()
 {
-	MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    InitWindow(800, 600, "GABDICE");
 
-	if (!device) {
-		std::cout << "Failed to create Metal device\n";
-		return -1;
-	}
+    Camera camera = { 0 };
+    camera.position = { 15,15,15 };
+    camera.target = { 0,0,0 };
+    camera.up = { 0,1,0 };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
 
-	std::cout << "Metal device created\n";
+    Model model = LoadModel("../res/dice.glb");
 
-	RegisterDefaultAllocator();
+    RegisterDefaultAllocator();
 
-	Factory::sInstance = new Factory();
-	JPH::RegisterTypes();
+    Factory::sInstance = new Factory();
+    RegisterTypes();
 
-	TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
+    PhysicsSystem physics;
 
-	JobSystemThreadPool job_system(
-		cMaxPhysicsJobs,
-		cMaxPhysicsBarriers,
-		std::thread::hardware_concurrency()
-	);
+    BroadPhaseLayerInterfaceImpl broadPhaseLayerInterface;
+    ObjectVsBroadPhaseLayerFilterImpl objectVsBroadPhaseLayerFilter;
+    ObjectLayerPairFilterImpl objectLayerPairFilter;
 
-	BroadPhaseLayerInterfaceImpl broad_phase_layer_interface;
-	ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter;
-	ObjectLayerPairFilterImpl object_layer_pair_filter;
+    physics.Init(
+        1024, 0, 1024, 1024,
+        broadPhaseLayerInterface,
+        objectVsBroadPhaseLayerFilter,
+        objectLayerPairFilter
+    );
 
-	PhysicsSystem physics;
+    physics.SetGravity(Vec3(0, -9.81f, 0));
 
-	physics.Init(
-		1024,
-		0,
-		1024,
-		1024,
-		broad_phase_layer_interface,
-		object_vs_broadphase_layer_filter,
-		object_layer_pair_filter
-	);
+    BodyInterface& bodyInterface = physics.GetBodyInterface();
 
-	physics.SetGravity(Vec3(0, -9.81f, 0));
+    RefConst<Shape> floorShape = new BoxShape(Vec3(10, 0.5f, 10));
 
-	BoxShapeSettings box(Vec3(1.0f, 1.0f, 1.0f));
-	auto shape_result = box.Create();
-	RefConst<Shape> shape = shape_result.Get();
+    BodyCreationSettings floorSettings(
+        floorShape,
+        Vec3(0, -1, 0),
+        Quat::sIdentity(),
+        EMotionType::Static,
+        Layers::NON_MOVING
+    );
 
-	BodyCreationSettings body_settings(
-		shape,
-		Vec3(0, 10, 0),
-		Quat::sIdentity(),
-		EMotionType::Dynamic,
-		Layers::MOVING
-	);
+    Body* floor = bodyInterface.CreateBody(floorSettings);
+    bodyInterface.AddBody(floor->GetID(), EActivation::DontActivate);
 
-	BodyInterface& body_interface = physics.GetBodyInterface();
+    RefConst<Shape> diceShape = new BoxShape(Vec3(0.1f, 0.1f, 0.1f));
 
-	BodyID body_id = body_interface.CreateAndAddBody(
-		body_settings,
-		EActivation::Activate
-	);
+    BodyCreationSettings diceSettings(
+        diceShape,
+        Vec3(0, 5, 0),
+        Quat::sIdentity(),
+        EMotionType::Dynamic,
+        Layers::MOVING
+    );
 
-	for (int i = 0; i < 120; i++)
-	{
-		physics.Update(1.0f / 60.0f, 1, &temp_allocator, &job_system);
+    Body* diceBody = bodyInterface.CreateBody(diceSettings);
+    bodyInterface.AddBody(diceBody->GetID(), EActivation::Activate);
 
-		Vec3 pos = body_interface.GetPosition(body_id);
+    TempAllocatorImpl tempAllocator(10 * 1024 * 1024);
 
-		std::cout << "Y: " << pos.GetY() << std::endl;
-	}
+    JobSystemThreadPool jobSystem(
+        cMaxPhysicsJobs,
+        cMaxPhysicsBarriers,
+        std::thread::hardware_concurrency() - 1
+    );
 
-	delete Factory::sInstance;
+    SetTargetFPS(60);
 
-	return 0;
+    while (!WindowShouldClose())
+    {
+        float dt = GetFrameTime();
+
+        physics.Update(dt, 1, &tempAllocator, &jobSystem);
+
+        RVec3 pos = bodyInterface.GetPosition(diceBody->GetID());
+        Quat rot = bodyInterface.GetRotation(diceBody->GetID());
+
+        Vector3 position = { (float)pos.GetX(), (float)pos.GetY(), (float)pos.GetZ() };
+
+        Vec3 axis{};
+        float angle;
+        rot.GetAxisAngle(axis, angle);
+
+        Vector3 axisRL = { axis.GetX(), axis.GetY(), axis.GetZ() };
+
+        BeginDrawing();
+        ClearBackground(::RAYWHITE);
+
+        BeginMode3D(camera);
+
+        DrawModelEx(
+            model,
+            position,
+            axisRL,
+            angle * RAD2DEG,
+            (Vector3){ 0.01f,0.01f,0.01f },
+            ::WHITE
+        );
+
+        DrawCube((Vector3){0,-1,0}, 20, 1, 20, ::GRAY);
+        DrawGrid(10, 1.0f);
+
+        EndMode3D();
+
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+
+    UnloadModel(model);
+    CloseWindow();
+
+    return 0;
 }
