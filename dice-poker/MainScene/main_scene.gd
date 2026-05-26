d2b0d63cb56ea322d32b519428a66e0c42c452df
Gabrax <gab-oz@wp.pl>
2026-05-26 12:55:25 +0200

    ui changes, project structure changed

--- dice-poker/MainScene/main_scene.gd
+++ dice-poker/MainScene/main_scene.gd
@@ -2,16 +2,23 @@ extends Node3D
 
 @export var dice_scene: PackedScene
 
+@onready var charge_bar = get_node("/root/MainScene/UI/ProgressBar")
+
 var charge := 0.0
 
-const MAX_FORCE := 0.01
-const CHARGE_SPEED := 0.01
+const MAX_FORCE := 0.1
+const CHARGE_SPEED := 0.1
 
 func _process(delta):
 
 	if Input.is_action_pressed("ui_accept"):
+
 		charge += CHARGE_SPEED * delta
 		charge = clamp(charge, 0.0, MAX_FORCE)
+
+		# Update UI
+		charge_bar.value = (charge / MAX_FORCE) * 100.0
+
 
 	if Input.is_action_just_released("ui_accept"):
 
@@ -19,24 +26,28 @@ func _process(delta):
 
 		charge = 0.0
 
+		# Reset bar
+		charge_bar.value = 0
+
+
 func throw_dice(force):
 
 	var dice = dice_scene.instantiate()
 
 	add_child(dice)
 
-	dice.global_position = Vector3(0,10,0)
+	dice.global_position = Vector3(0, 10, 0)
 
 	var dir = Vector3(
-		randf_range(-0.2,0.2),
+		randf_range(-0.2, 0.2),
 		-1,
-		randf_range(-0.2,0.2)
+		randf_range(-0.2, 0.2)
 	).normalized()
 
 	dice.apply_impulse(dir * force)
 
 	dice.apply_torque_impulse(Vector3(
-		randf_range(-10,10),
-		randf_range(-10,10),
-		randf_range(-10,10)
+		randf_range(-10, 10),
+		randf_range(-10, 10),
+		randf_range(-10, 10)
 	))
---
+++ dice-poker/MainScene/ui.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var menu = get_node("/root/MainScene/UI/MENU")
+@onready var menu_options = get_node("/root/MainScene/UI/MENU_OPTIONS")
+
+@onready var menu_play_button = get_node("/root/MainScene/UI/MENU/VBoxContainer/button_play")
+@onready var menu_options_button = get_node("/root/MainScene/UI/MENU/VBoxContainer/button_options")
+@onready var menu_quit_button = get_node("/root/MainScene/UI/MENU/VBoxContainer/button_quit")
+
+@onready var options_return_button = get_node("/root/MainScene/UI/MENU_OPTIONS/VBoxContainer/button_return")
+
+
+func _ready() -> void:
+
+	menu.show()
+	menu_options.hide()
+
+	menu_play_button.pressed.connect(_on_play_pressed)
+	menu_options_button.pressed.connect(_on_options_pressed)
+	menu_quit_button.pressed.connect(_on_quit_pressed)
+
+	options_return_button.pressed.connect(_on_return_pressed)
+
+
+func _on_play_pressed() -> void:
+
+	menu.hide()
+	menu_options.hide()
+
+
+func _on_options_pressed() -> void:
+
+	menu.hide()
+	menu_options.show()
+
+
+func _on_quit_pressed() -> void:
+
+	get_tree().quit()
+
+
+func _on_return_pressed() -> void:
+
+	menu_options.hide()
+	menu.show()
